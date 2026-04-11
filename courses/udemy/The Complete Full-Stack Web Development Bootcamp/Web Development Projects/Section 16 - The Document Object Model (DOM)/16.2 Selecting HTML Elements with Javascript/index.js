b4642a9f6efac3990349627eed5df00663d42b1c
Abismorfis <256637702+Abismorfis@users.noreply.github.com>
2026-04-11 20:39:12 +0100

    [Project] Add DOM element selection and manipulation exercise in the Section 16 - The Document Object Model (DOM)

---
+++ courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 16 - The Document Object Model (DOM)/16.2 Selecting HTML Elements with Javascript/index.js
@@ -0,0 +1,9 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Ryan";
+
+document.querySelector('li a').style.color = "Red";
+
+document.querySelector('ul').lastElementChild.style.color = "blue";
+
+function checkboxEvent() {
+    document.querySelector('input').click();
+};
