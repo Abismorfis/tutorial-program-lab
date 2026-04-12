5255a6b1d29cac718645ccfee1855a1ec53c6dd7
Abismorfis <256637702+Abismorfis@users.noreply.github.com>
2026-04-12 20:55:27 +0100

    [Project] Add DOM manipulation and styling exercise with JavaScript in the Section 16 - The Document Object Model (DOM)

---
+++ courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 16 - The Document Object Model (DOM)/16.3 Manipulating and Changing Styles of HTML Elements with Javascript/index.js
@@ -0,0 +1,13 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Ryan";
+
+document.querySelector('li a').style.color = "Red";
+
+document.querySelector('ul').lastElementChild.style.color = "blue";
+
+document.querySelector('ul').lastElementChild.style.fontSize = "20px";
+
+document.querySelector('button').style.backgroundColor = "yellow";
+
+function checkboxEvent() {
+    document.querySelector('input').click();
+};
