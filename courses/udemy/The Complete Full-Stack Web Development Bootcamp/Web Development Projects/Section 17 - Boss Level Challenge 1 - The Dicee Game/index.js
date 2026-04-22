cedf43ba194fb8d93b422e9c297c5bc50abffca7
Abismorfis <256637702+Abismorfis@users.noreply.github.com>
2026-04-22 20:55:24 +0100

    [Project] Add feat: dynamically set first dice image based on random number in the Section 17 - Boss Level Challenge 1 - The Dicee Game

--- courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 17 - Boss Level Challenge 1 - The Dicee Game/index.js
+++ courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 17 - Boss Level Challenge 1 - The Dicee Game/index.js
@@ -1,3 +1,10 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 console.log(randomNumber1);
+
+const qualifiedName = 'src';
+const qualifiedvalue = `./images/dice${randomNumber1}.png`;
+
+const image1 = document.querySelector('.img1');
+
+image1.setAttribute(qualifiedName, qualifiedvalue);
