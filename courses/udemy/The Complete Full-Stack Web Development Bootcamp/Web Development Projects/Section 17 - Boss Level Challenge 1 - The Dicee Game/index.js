26968e7d84dff8e2f3823427a8d71695439b81ca
Abismorfis <256637702+Abismorfis@users.noreply.github.com>
2026-04-23 20:51:15 +0100

    [Project] Add second dice with random number generation and image display in the Section 17 - Boss Level Challenge 1 - The Dicee Game

--- courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 17 - Boss Level Challenge 1 - The Dicee Game/index.js
+++ courses/udemy/The Complete Full-Stack Web Development Bootcamp/Web Development Projects/Section 17 - Boss Level Challenge 1 - The Dicee Game/index.js
@@ -1,10 +1,15 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 console.log(randomNumber1);
+console.log(randomNumber2);
 
 const qualifiedName = 'src';
-const qualifiedvalue = `./images/dice${randomNumber1}.png`;
+const qualifiedvalue1 = `./images/dice${randomNumber1}.png`;
+const qualifiedvalue2 = `./images/dice${randomNumber2}.png`;
 
 const image1 = document.querySelector('.img1');
+const image2 = document.querySelector('.img2');
 
-image1.setAttribute(qualifiedName, qualifiedvalue);
+image1.setAttribute(qualifiedName, qualifiedvalue1);
+image2.setAttribute(qualifiedName, qualifiedvalue2);
